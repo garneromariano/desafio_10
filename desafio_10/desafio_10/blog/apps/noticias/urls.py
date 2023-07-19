019157b33cec1d2ec81543b64d3141929a60eb94
garneromariano <138075153+garneromariano@users.noreply.github.com>
2023-07-19 16:35:44 -0300

    crear noticias y CRUD para la app noticias

--- desafio_10/desafio_10/blog/apps/noticias/urls.py
+++ desafio_10/desafio_10/blog/apps/noticias/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
+from .views import *
 from . import views
 
 app_name = 'noticias'
 
 # URL de app noticias
 urlpatterns = [
-    path('',views.inicio,name='inicio'),
-    
-    # url para el detalle de la noticia por pk
-    path('detalle<int:pk>', views.Detalle_Noticias, name='detalle')
+   
+
+
+    path('', views.inicio, name='inicio'),
+    path('detalle/<int:pk>/', views.Detalle_Noticias, name='detalle'),  # Agrega una barra antes de <int:pk>
+    path('crear_noticia/', views.crear_noticia, name='crear_noticia'),  # Asegúrate de tener una barra después de crear_noticia
+    path('listar/', views.listar_noticias, name='listar_noticias'),
+    path('<int:pk>/', views.ver_noticia, name='ver_noticia'),
+    path('<int:pk>/editar/', views.editar_noticia, name='editar_noticia'),
+    path('<int:pk>/eliminar/', views.eliminar_noticia, name='eliminar_noticia'),
     ]
+
+    
+
